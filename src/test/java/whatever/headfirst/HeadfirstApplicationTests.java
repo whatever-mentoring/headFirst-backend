e5a1882425fbaf18ca50bc38d2ce2dbacae61a67
devholic22 <hyunjoon.tech@gmail.com>
2023-08-30 22:38:37 +0900

    initial commit

---
+++ src/test/java/whatever/headfirst/HeadfirstApplicationTests.java
@@ -0,0 +1,13 @@
+package whatever.headfirst;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HeadfirstApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
