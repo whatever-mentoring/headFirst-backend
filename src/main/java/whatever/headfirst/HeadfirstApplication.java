99d439469369c4137c926736f7125372d571eb66
devholic22 <hyunjoon.tech@gmail.com>
2023-09-09 16:25:53 +0900

    fix: EnableJpaAuditing 수정

--- src/main/java/whatever/headfirst/HeadfirstApplication.java
+++ src/main/java/whatever/headfirst/HeadfirstApplication.java
@@ -2,9 +2,11 @@ package whatever.headfirst;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
-public class HeadfirstApplication {
+class HeadfirstApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(HeadfirstApplication.class, args);
