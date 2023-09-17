330e64cf0d63b3ea2226cdcdd0dfee74b658cbbf
devholic22 <hyunjoon.tech@gmail.com>
2023-09-17 15:18:32 +0900

    fix: findByOwnerIdAndStoryId 수정

--- src/main/java/whatever/headfirst/domain/heart/repository/HeartRepository.java
+++ src/main/java/whatever/headfirst/domain/heart/repository/HeartRepository.java
@@ -4,9 +4,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import whatever.headfirst.domain.heart.domain.Heart;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface HeartRepository extends JpaRepository<Heart, Long> {
 
     List<Heart> findAllByStoryId(Long storyId);
-    List<Heart> findByOwnerIdAndStoryId(Long ownerId, Long storyId);
+    Optional<Heart> findByOwnerIdAndStoryId(Long ownerId, Long storyId);
 }
