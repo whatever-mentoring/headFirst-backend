1e99aa69ee4726eb189e8e76b9f56f9e6e849489
devholic22 <hyunjoon.tech@gmail.com>
2023-09-23 15:15:52 +0900

    fix: 401 해결

--- src/main/java/whatever/headfirst/global/config/SecurityConfig.java
+++ src/main/java/whatever/headfirst/global/config/SecurityConfig.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.dao.DaoAuthenticationProvider
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityCustomizer;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
@@ -45,6 +46,11 @@ public class SecurityConfig {
     }
 
     @Bean
+    public WebSecurityCustomizer webSecurityCustomizer() {
+        return (web) -> web.ignoring().requestMatchers("/auth/**", "/redirect/**", "/onboard/**");
+    }
+
+    @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
 
         http
@@ -56,12 +62,6 @@ public class SecurityConfig {
                     exception.accessDeniedHandler(jwtAccessDeniedHandler);
                 })
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
-                .authorizeHttpRequests(request -> {
-                    request.requestMatchers("/auth/**").permitAll();
-                    request.requestMatchers("/onboard/**").permitAll();
-                    request.requestMatchers("/redirect/**").permitAll();
-                    request.anyRequest().authenticated();
-                })
                 .addFilterBefore(
                         new JwtFilter(tokenProvider, memberRepository), UsernamePasswordAuthenticationFilter.class
                 );
